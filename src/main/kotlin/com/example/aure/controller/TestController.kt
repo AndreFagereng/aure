0a2f259343664084f5304491e82ef89d2920f3cc
Andre <andre_fagereng@hotmail.com>
2022-10-19 21:51:40 +0200

    security

--- src/main/kotlin/com/example/aure/controller/TestController.kt
+++ src/main/kotlin/com/example/aure/controller/TestController.kt
@@ -1,12 +1,26 @@
 package com.example.aure.controller
 
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
+
     @GetMapping("/ping")
     fun pingPong(): String {
         return "pong"
+    }
+    @GetMapping("/api/public")
+    fun a(): String {
+        return "/api/public"
+    }
+    @GetMapping("/api/private")
+    fun b(): String {
+        return "/api/private"
+    }
+    @GetMapping("/api/private-scoped")
+    fun c(): String {
+        return "/api/private-scoped"
     }
 }
